b1f6181d425735c1f5ff704fd96f80d630a9b2c3
faireenazaidi <168419753+faireenazaidi@users.noreply.github.com>
2024-09-20 18:54:59 +0530

    faireena

--- lib/PeerCircle/AddFriends/AddFriendView.dart
+++ lib/PeerCircle/AddFriends/AddFriendView.dart
@@ -1,101 +1,197 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
-import 'package:namaz_reminders/PeerCircle/AddFriends/AddFriendController.dart';
+import 'package:namaz_reminders/Widget/myButton.dart';
+import 'AddFriendController.dart';
+import 'AddFriendDataModal.dart';
 import 'package:namaz_reminders/Widget/appColor.dart';
 import 'package:namaz_reminders/Widget/text_theme.dart';
 
-import 'AddFriendDataModal.dart';
-
 class AddFriendView extends GetView<AddFriendController> {
+  const AddFriendView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final AddFriendController controller = Get.put(AddFriendController());
+    //final AddFriendController controller = Get.put(AddFriendController());
+
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         centerTitle: true,
-          title: Text('Invite friends',style: MyTextTheme.mediumBCD,),
-      leading: InkWell(
-        onTap: (){
-          Get.back();
-        },
-          child: Icon(Icons.arrow_back_ios_new)),),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            buildSearchBar(),
-            buildSection(title: 'REQUESTS', list: controller.requests, actionLabel: 'Accept', action: controller.acceptRequest),
-            buildSection(title: 'IN YOUR CONTACT LIST', list: controller.contacts, actionLabel: 'Invite', action: controller.invitePerson),
-            buildSection(title: 'PEOPLE NEARBY', list: controller.nearbyPeople, actionLabel: 'Invite', action: controller.invitePerson),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildSearchBar() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Search username...',
-          prefixIcon: Icon(Icons.search),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-        ),
-      ),
-    );
-  }
-
-  Widget buildSection({required String title, required RxList<Person> list, required String actionLabel, required Function(Person) action}) {
-    return Obx(() => Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(title, style: MyTextTheme.greyNormal),
-        ),
-        ListView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            final person = list[index];
-            return Column(
-              children: [
-                ListTile(
-                  leading: CircleAvatar(backgroundImage: person.imageUrl != null ? AssetImage(person.imageUrl!) : null),
-                  title: Text(person.name),
-                  subtitle: Text(person.phoneNumber),
-                   trailing:
-                   //TextButton(
-                  //   onPressed: () => action(person),
-                  //   child: Text(actionLabel, style: TextStyle(color: AppColor.circleIndicator)),
-                  // ),
-                    Container(
-                      height:40,
-
-                      decoration: BoxDecoration(
-                        color: AppColor.circleIndicator,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextButton(
-                        onPressed: () =>
-                          action(person),
-                        child: Text(actionLabel, style: TextStyle(color: AppColor.white)),
-
-                      ),
-                    )
-                ),
-                if (index < list.length - 5) Divider(), // Add Divider between items, but not after the last item
-              ],
-            );
+        title: Text('Invite friends', style: MyTextTheme.mediumBCD),
+        leading: InkWell(
+          onTap: () {
+            Get.back();
           },
+          child: Icon(Icons.arrow_back_ios_new),
         ),
-        Divider(), // Add a Divider after the section
-      ],
-    ));
+      ),
+      body:
+      GetBuilder(
+        init: controller,
+        builder: (_) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  // controller: controller.nameC.value,
+                  cursorColor: AppColor.circleIndicator,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "Search Username..",
+                    hintStyle: MyTextTheme.mediumCustomGCN,
+                    // prefixIcon: Image.asset("asset/profile.png"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide:  BorderSide(
+                        color: Colors.black,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Friend Reuests",style: MyTextTheme.largeBCB,),
+                  ],
+                ),
+
+                Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: controller.getFriendRequestList.length,
+                      itemBuilder: (context, index) {
+                        FriendRequestDataModal friendRequestData = controller.getFriendRequestList[index];
+                        print("!!!!!!!!!!"+friendRequestData.name.toString());
+
+                        return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                radius: 20,
+                                child: Icon(Icons.person,color: Colors.white,),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 12.0,top: 12),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(friendRequestData.id.toString(),style: MyTextTheme.mediumGCB.copyWith(fontSize: 16,color: Colors.black,fontWeight: FontWeight.bold ),),
+                                    Text(friendRequestData.mobileNo.toString(),style: MyTextTheme.mediumGCB.copyWith(fontSize: 14,),),
+
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          InkWell(
+                            onTap: () async {
+                              await controller.fetchFriendRequests();
+                            },
+                            child: Container(
+                              height: 30,width: 80,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: AppColor.white),
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColor.circleIndicator
+                              ),
+                               child: Center(child: Text("Accept",style: TextStyle(color: Colors.white),)),
+                            ),
+                          )
+                        ],
+                      );
+                    },)),  Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Registered User",style: MyTextTheme.largeBCB,),
+                  ],
+                ),
+
+
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: controller.getRegisteredUserList.length,
+                    itemBuilder: (context, index) {
+                      RegisteredUserDataModal registeredData = controller.getRegisteredUserList[index];
+                      print("?????????"+registeredData.name.toString());
+
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                radius: 20,
+                                child: Icon(Icons.person,color: Colors.white,),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 12.0,top: 12),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(registeredData.name.toString(),style: MyTextTheme.mediumGCB.copyWith(fontSize: 16,color: Colors.black,fontWeight: FontWeight.bold ),),
+                                    Text(registeredData.mobileNo.toString(),style: MyTextTheme.mediumGCB.copyWith(fontSize: 14,),),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          InkWell(
+                            onTap: () async {
+                              await controller.sendFriendRequest(registeredData);
+                            },
+                            child: Container(
+                              height: 30,width: 80,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: AppColor.white),
+                                borderRadius: BorderRadius.circular(10),
+                                color: AppColor.circleIndicator
+                              ),
+                              child: Center(child: Text("Invite",style: TextStyle(color: Colors.white),)),
+                            ),
+                          )
+
+
+                        ],
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          );
+        }
+      ),
+
+
+
+    );
   }
+
+
 }
